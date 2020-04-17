191a218ec6eb7c6d7bdb73b3917f4dc9dd4ba6a5
Benjamin Patrick <61899689+bhenjameen@users.noreply.github.com>
2020-04-17 08:33:38 +0100

    Update BankingSystem.rb

--- BankingSystem.rb
+++ BankingSystem.rb
@@ -1,27 +1,57 @@
 #Welcome interface for user
-puts ("\n" * 2)
-puts "Welcome to the START.NG Banking app! \nWe make Banking fun for you!"
+def welcomeInterface
+    puts ("\n" * 2)
+    sleep 1.0
+    puts "..."
+    sleep 1.0
+    puts "Welcome to the START.NG Banking app! \nWe make Banking fun for you!"
 
-puts ("\n" * 2)
-puts "What would you like to do?. \nChoose your desired option."
-puts ("\n" * 2)
-
-puts "STAFF LOGIN:  (Press '1' and 'Enter' to log in as Staff)"
-puts "CLOSE APP:    (Press '2' and 'Enter' to exit the app)"
-puts ("\n" * 2)
-response = gets.chomp
+    puts ("\n" * 2)
+    sleep 1.0
+    puts "..."
+    sleep 1.0
+    puts "What would you like to do?. \nChoose your desired option."
+    puts ("\n")
+    sleep 1.0
+ 
+    puts "STAFF LOGIN:  (Press '1' and 'Enter' to log in as Staff)"
+    puts "CLOSE APP:    (Press '2' and 'Enter' to exit the app)"
+    puts ("\n" * 2)
+    response = gets.chomp
+    if response == "1" then
+        puts    
+        sleep 1.0
+        puts "..."
+        sleep 1.0
+        puts "To access your account, input your username and password below"
+        userValidation
+        elsif response == "2" then
+        closeApp
+    end 
+end
 
 
 def userValidation
-    print "\n\nPlease input your USERNAME:  "
-    usernameValidation = gets.chomp().to_s
+    puts
+    sleep 1.0
+    puts "..."
+    sleep 1.0
+    print "Please input your USERNAME:  "
+    usernameValidation = gets.chomp
+    puts
+    sleep 1.0
+    puts "..."
+    sleep 1.0
     print "Please input your PASSWORD:  "
-    passwordValidation = gets.chomp().to_s
+    passwordValidation = gets.chomp
 
     File.open("staff.txt", "r") do |file|
     if file.read().include? usernameValidation && passwordValidation
-        puts "\n\nLogin successful"
-        puts "\n\nWelcome Back. \nChoose your desired option"
+        sleep 1.0
+        puts "\n\n..."
+        sleep 1.0
+        puts "Login successful"
+        puts "\nWelcome Back. \nChoose your desired option"
         userProgress
     else
         pinError
@@ -42,11 +72,56 @@ def userProgress
         accountDetails
     elsif
         staffResponse == "3" then
-        closeApp
+        puts "\n\nYou have successfully logged out"
+        welcomeInterface
     else
         puts "You have made an invalid selection. \nPlease try again\n\n"
         userProgress
     end
+end
+
+
+def createNewAccount
+    sleep 1.0
+    puts "\n\n..."
+    sleep 1.0
+    puts "Welcome to START.NG Bank. \nPlease fill the following data form..."
+    sleep 1.0
+    puts "\n..."
+    sleep 1.0
+    print "ACCOUNT NAME:   "
+    accName = gets.chomp
+    sleep 1.0
+    puts "\n..."
+    sleep 1.0
+    print "OPENING BALANCE:   "
+    openBal = gets.chomp    
+    sleep 1.0
+    puts "\n..."
+    sleep 1.0
+    print "ACCOUNT TYPE:   "
+    accType = gets.chomp
+    sleep 1.0
+    puts "\n..."
+    sleep 1.0
+    print "ACCOUNT EMAIL:   "
+    accEmail = gets.chomp
+    accNum = 10.times.map{rand(0..9)}.join.to_s
+    puts "Your account number is " + accNum
+
+    File.open("customer.txt", "a") do |file|
+        file.write("Account name: " + accName + "\n")
+        file.write("Account Balance: " + openBal + "\n")
+        file.write("Account type: " + accType + "\n")
+        file.write("Account email: " + accEmail + "\n")
+        file.write("Account number: " + accNum + "\n")
+    end
+    puts    
+    sleep 1.0
+    puts "..."
+    sleep 1.0
+    puts "\nWelcome Back. \nChoose your desired option"
+    userProgress
 end
 
 
@@ -56,15 +131,14 @@ end
 
 
 def pinError
-    puts "\n\nError! Invalid Password or Username, please try again."
+    puts
+    sleep 1.0
+    puts "..."
+    sleep 1.0
+    puts "Error! Invalid Password or Username, please try again."
     userValidation
 end
 
 
-if response == "1" then
-    puts "\n\nTo access your account, input your username and password below"
-    userValidation
-    elsif response == "2" then
-    closeApp
-end 
 
+welcomeInterface
